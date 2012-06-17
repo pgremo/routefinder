2b2523f41e956a26788d978648718786f2fc0c53
Patrick Gremo <patrick.gremo@gmail.com>
2012-06-17 12:11:23 -0500

    added sorting of waypoints

--- resources/public/js/route.js
+++ resources/public/js/route.js
@@ -1,19 +1,19 @@
 $(function() {
-    function log(message) {
-      var html = '<li><input type="hidden" id="waypoint-{0}" name="waypoint[]" value="{0}" /><span class="waypoint-label">{0}</span><a class="waypoint-delete" href="#">X</a></li>';
-      $("#waypoints").append(html.format(message));
-    }
-
     $("#waypoints").on("click", ".waypoint-delete", function(){
       $(this).closest("li").remove();
       return false;
     });
+    $("#waypoints").sortable().disableSelection();
 
     $("#solarsystem").autocomplete({
       source: "/solarsystem/search.json",
       minLength: 2,
       select: function(event, ui) {
-        if (ui.item) log(ui.item.value)
+        if (ui.item){
+          var html = '<li><input type="hidden" id="waypoint-{0}" name="waypoint[]" value="{0}" /><span class="waypoint-label">{0}</span><a class="waypoint-delete" href="#">X</a></li>';
+          $("#waypoints").append(html.format(ui.item.value));
+          $("#waypoints").sortable("refresh");
+        }
       }
     });
 
