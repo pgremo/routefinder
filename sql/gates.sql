823e3f7e86dd9e10007cacded31d7eebd3bb5757
pmgremo <pmgremo@thoughtworks.com>
2012-06-19 13:36:07 -0700

    simplified gates.sql

--- sql/gates.sql
+++ sql/gates.sql
@@ -1,12 +1,12 @@
 select s.itemid source, s.solarsystemid,
-    case when s.itemid = d.itemid then sj.celestialid else d.itemid end destination,
+    case when s.itemid = d.itemid then o.itemid else d.itemid end destination,
     case when s.itemid = d.itemid then o.security else d.security end security,
     case
         when s.itemid = d.itemid then 1
-        when s.solarsystemid= d.solarsystemid then sqrt(power(s.x - d.x, 2) + power(s.y - d.y, 2) + power(s.z - d.z, 2)) / 149598000000
+        else sqrt(power(s.x - d.x, 2) + power(s.y - d.y, 2) + power(s.z - d.z, 2)) / 149598000000
     end cost
 from static.mapdenormalize s
 join static.mapjumps sj on s.itemid = sj.stargateid
 join static.mapdenormalize d on s.solarsystemid = d.solarsystemid
 join static.mapjumps dj on d.itemid = dj.stargateid
-join static.mapdenormalize o on dj.celestialid = o.itemid
+join static.mapdenormalize o on sj.celestialid = o.itemid
