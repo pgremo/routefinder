6b33bfd369c89764f3cc8e1f8a0e4b1318221572
pmgremo <pmgremo@thoughtworks.com>
2012-06-20 18:31:35 -0700

    added costs to output

--- src/routefinder/views/route.clj
+++ src/routefinder/views/route.clj
@@ -11,21 +11,27 @@
   [nodes]
   (->>
     (solve nodes)
-    (drop 25)
-    (first)
+    (nth 26)
     (route)
     (map rest)
     (flatten)
-    (map (comp :SOLARSYSTEMNAME solarsystems/by-id :SOLARSYSTEMID gates/by-id))
-    (map (juxt (partial in? nodes) identity))))
+    (map (comp (juxt :SOLARSYSTEMID :COST) gates/by-id))
+    (map (juxt (partial in? nodes) (comp :SOLARSYSTEMNAME solarsystems/by-id :SOLARSYSTEMID gates/by-id) (comp :COST gates/by-id)))))
+
+(defn route-finder2
+  [nodes]
+  (let [segments (route (nth 26 (solve nodes)))]
+    (for [[first second] (map () segments (rest segments))]
+      [])))
 
 (defsnippets "templates/route.html"
   (form [:form ] [])
   (header [:head :> :* ] [])
-  (result [:table ] [nodes] [:tr ] (clone-for [[index [waypoint segment]] (map-indexed #(vec %&) nodes)]
+  (result [:table ] [nodes] [:tr ] (clone-for [[index [waypoint segment cost]] (map-indexed #(vec %&) nodes)]
                                      [[:td (nth-child 1)]] (content (String/valueOf index))
                                      [[:td (nth-child 2)]] (content (String/valueOf waypoint))
-                                     [[:td (nth-child 3)]] (content (String/valueOf segment)))))
+                                     [[:td (nth-child 3)]] (content (String/valueOf segment))
+                                     [[:td (nth-child 4)]] (content (String/valueOf cost)))))
 
 (defpage [:post "/route"] {:keys [waypoint]}
   (layout (header) (result (route-finder (map #(Long/valueOf %) waypoint)))))
